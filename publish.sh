4f4f3143e5f4542a8e07275de4af11dfaae7e9f9
donate-initiative <72306016+donate-initiative@users.noreply.github.com>
2020-11-08 19:23:01 -0500

    Update publish.sh
    changed line 5 from tech-portfolio to overview

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 set -e
 
 INPUT_BRANCH=${INPUT_BRANCH:-gh-pages}
-INPUT_DIRECTORY=${INPUT_DIRECTORY:-'website/build/donate-initiative'}
+INPUT_DIRECTORY=${INPUT_DIRECTORY:-'website/build/overview'}
 
 echo "Push to branch $INPUT_BRANCH";
 [ -z "${GITHUB_TOKEN}" ] && {
